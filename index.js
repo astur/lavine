11a24e6e00be9993f09ed5925185426f7c914692
astur <astur@yandex.ru>
2018-07-15 07:30:24 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = () => {};
+const whiler = require('whiler');
+
+module.exports = source => {
+    const getWorker = () => source && source.shift ? source.shift() || null : null;
+    const thread = () => {
+        const worker = getWorker();
+        if(worker === null) return false;
+        return whiler(worker).then(
+            () => false,
+            () => true,
+        );
+    };
+    return whiler(thread);
+};
