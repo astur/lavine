160e45f38b6b153f7903507636eca341519c1af0
astur <astur@yandex.ru>
2018-07-15 07:43:40 +0300

    test for bad source

--- test.js
+++ test.js
@@ -2,5 +2,7 @@ const test = require('ava');
 const lavine = require('.');
 
 test('empty', async t => {
+    await lavine(null).then(() => t.pass());
+    await lavine('bad').then(() => t.pass());
     await lavine([]).then(() => t.pass());
 });
