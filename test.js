ac382390eff38d16ca64106a86a9fa47b5ea2f69
astur <astur@yandex.ru>
2018-07-15 07:44:07 +0300

    test for simple single source

--- test.js
+++ test.js
@@ -6,3 +6,8 @@ test('empty', async t => {
     await lavine('bad').then(() => t.pass());
     await lavine([]).then(() => t.pass());
 });
+
+test('single', async t => {
+    await lavine([() => false]).then(() => t.pass());
+    await lavine([() => ''.throwHere()]).then(() => t.pass());
+});
