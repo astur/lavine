e4f7b435ab8b4ae8b448c781c40bc640dcdc8a1d
astur <astur@yandex.ru>
2018-07-23 15:45:01 +0300

    fix async getWorker

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = (source, concurrency) => {
     if(Array.isArray(source) && (concurrency === 0 || concurrency > source.length)) concurrency = source.length;
     if(!Number.isInteger(concurrency) || concurrency < 0) concurrency = 1;
     const getWorker = Array.isArray(source) ? () => source.shift() || null : source;
-    const thread = () => {
-        const worker = getWorker();
+    const thread = async () => {
+        const worker = await getWorker();
         if(worker === null) return false;
         return whiler(worker).then(
             () => false,
